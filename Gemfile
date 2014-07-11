904289922517778ff09246dc8594709adc0f53e3
kid84 <84takuya@gmail.com>
2014-07-11 23:40:54 +0900

    sinatraBase

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# A sample Gemfile
+source "https://rubygems.org"
+gem "thin"
+
+gem "sinatra",require:"sinatra/base"
+gem "sinatra-contrib",require:"sinatra/contrib/all"
+
+gem "haml"
+gem "slim"
+gem "sass"
+gem "compass"
+gem "coffee-script"
---
+++ app.rb
@@ -0,0 +1,40 @@
+require 'sinatra'
+require 'compass'
+
+set :environment, :production
+class App < Sinatra::Base
+	register Sinatra::Reloader
+
+	# Compassの設定
+	configure do
+    	Compass.configuration do |config|
+      		config.project_path = File.dirname(__FILE__)
+      		config.sass_dir = 'views'
+    	end
+    	set :haml, { format: :html5 }
+    	set :sass, Compass.sass_engine_options
+    	set :scss, Compass.sass_engine_options
+    end
+
+	
+	# top
+	get '/' do
+		haml:'index'
+	end
+	get '/script.js' do
+		coffee:'coffee/script'
+	end
+	get '/style.css' do
+		# scss:'scss/style',:style => :compressed,:line_comments => false,:cache=>false
+		scss:'scss/style',:style => :expanded,:line_comments => false,:cache=>false
+	end
+
+	# test
+	get '/test' do
+		erb:'test/index'
+	end
+	get '/test/style.css' do
+		# scss:'scss/style',:style => :compressed,:line_comments => false,:cache=>false
+		scss:'test/style',:style => :expanded,:line_comments => false,:cache=>false
+	end
+end
